cd8177fa0ecb5ffaed9b9c0b0acac126b4e18bef
sirageDb <ilovesemicolons@hotmail.com>
2021-09-20 21:31:30 +0200

    FEAT : project editor interface FEAT : component tags organizer

--- src/components/backoffice/tagsOrganizer/TagsOrganizer.tsx
+++ src/components/backoffice/tagsOrganizer/TagsOrganizer.tsx
@@ -1,8 +1,37 @@
 import React from "react";
-export default function TagsOrganizer(): JSX.Element {
+import styles from "./tagsOrganizer.module.scss";
+import removeIcon from "../../../assets/removeIcon.svg";
+
+
+
+interface ITagsOrganizer {
+  tags: string[];
+}
+
+
+export default function TagsOrganizer({ tags }: ITagsOrganizer): JSX.Element {
+  const removeTag = (text : any)=> {
+    console.log(text);
+    
+  }
+
+
+  const Tag = ({text} : any) => {
+    return (
+      <div className={styles.singleTagContainer}>
+        <div className={styles.singleTag}>
+          <div>{text}</div>
+          <img className={styles.removeIcon} onClick={e => removeTag(e)} src={removeIcon} alt={"azeze"} />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <div>This is tags organizer</div>
+      {tags.map((tag: string, i) => {
+        return <Tag key={i} text={tag} />;
+      })}
     </div>
   );
 }
