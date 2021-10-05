702243a5fbdb21c1b3a299598a3a21d12a64c8f7
sirageDb <ilovesemicolons@hotmail.com>
2021-10-05 08:17:12 +0200

    FEAT : about page is ready

--- src/routes/about/About.tsx
+++ src/routes/about/About.tsx
@@ -1,5 +1,41 @@
 import React from "react";
 import PageLayout from "../../components/pageLayout/PageLayout";
-export default function About() : JSX.Element {
-    return (<PageLayout><div>sss</div></PageLayout>);
+import styles from "./about.module.scss";
+import me from "../../assets/me.svg";
+
+export default function About(): JSX.Element {
+  return (
+    <PageLayout contentMaxWidth={1100}>
+      <h1 className={styles.pageTitle}>About me</h1>
+      <div className={styles.contentContainer}>
+        <div className={styles.contentText}>
+          <div className={styles.section}>
+            <div className={styles.sectionHeading}>
+              <span className={styles.sectionTitle}>Who am i</span> .... Simple question …. complicated answer(s)
+            </div>
+            <div className={styles.sectionExplanation}>
+              My name isSirage Al dbiyat, I am a full stack web developer at KOLABS-STUDIO in Lyon, France. Also
+              interested in developing the dev community by sharing my knowledge, writing posts, and sharing my
+              opensource projects
+            </div>
+          </div>
+          <div className={styles.section}>
+            <span className={styles.sectionTitle}>Why semicolons</span>
+            <div className={styles.sectionExplanation}>
+              At the begining i named this website iLoveSemiColon, because obviously … i like semicolons, but then it
+              was time for a better name and i got the idea to simply name it Semicolons :)
+            </div>
+          </div>
+          <div className={styles.section}>
+            <span className={styles.sectionTitle}>A company ?, web agency ? , then what ?</span>
+            <div className={styles.sectionExplanation}>
+              Nope, as i said i work at KOLABS-STUDIO, Semicolons is not a company, nor a web agency, it is only my dev
+              blog where i can share with others my experience
+            </div>
+          </div>
+        </div>
+          <img  className={styles.imageContainer} src={me} alt={"sirage al dbiyat image personal website blog"} />
+      </div>
+    </PageLayout>
+  );
 }
