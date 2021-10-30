1e2d63d9729004395014af5b4851de89388470e1
SirageDb <ilovesemicolons@hotmail.com>
2021-10-30 02:01:31 +0200

    chore : homagepage redesign

--- src/routes/homepage/HomePage.tsx
+++ src/routes/homepage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./homepage.module.scss";
 import PageLayout from "../../components/pageLayout/PageLayout";
 import githubIcon from "../../assets/githubIcon.svg";
@@ -10,46 +10,41 @@ import apiEndPoint from "../../config/apiEndPoint";
 //images
 //====================
 import blob from "../../assets/blob.svg";
+import wavyHomePageBackground from "../../assets/wavyHomePageBackground.svg";
+import readerIllustration from "../../assets/reader.svg";
 
 //TODO an astroid from the sky when scrolling ...
 export default function HomePage(): JSX.Element {
-  
-  const [postData, setPostData] = useState<any>()
-  
+  const [postData, setPostData] = useState<any>();
+
   useEffect(() => {
     getLastPost();
-  }, [])
-
-
+  }, []);
 
   const getLastPost = async () => {
-    const apiResponse = await fetch(apiEndPoint + "/post/getposts?latest=true")
+    const apiResponse = await fetch(apiEndPoint + "/post/getposts?latest=true");
     const extractedData = await apiResponse.json();
     setPostData(extractedData);
-  }
+  };
 
-  
   const Tag = ({ text }: any) => {
     return <div className={styles.singleTag}># {text}</div>;
   };
 
-
-
-
   return (
     <PageLayout>
-      <div className={styles.pageHeading}>
-        <div className={styles.pageSloganContainer}>
-          <h1 className={styles.pageSlogan}>
-            Hey !, I am Sirage, welcome to my Semicolons zone where i share my tech knowledge
-          </h1>
-          <div className={styles.pageExpanation}>
-            As a full stack web developer i can work on various stages of a web or a mobile app development as well as
-            their full development lifecycle,
+      <div className={styles.pageHeadingContainer} style={{ backgroundImage: `url(${wavyHomePageBackground})` }}>
+        <div className={styles.pageHeading}>
+          <div className={styles.pageSloganContainer + " " + styles.rainbow}>
+            <h1 className={styles.pageSlogan}>
+              Hey !, I am Sirage, welcome to Semicolons zone where i share my tech knowledge
+            </h1>
+            <div className={styles.pageExpanation}>
+              As a full stack web developer i work on various stages of a web or a mobile app development as well as
+              their full development lifecycle,
+            </div>
           </div>
-        </div>
-        <div className={styles.contactWidgetsContainer}>
-          <div>
+          <div className={styles.contactWidgetsContainer}>
             <div className={styles.contactWidgetContainer + " " + styles.githubButton}>
               <a
                 className={styles.contactWidget}
@@ -65,8 +60,6 @@ export default function HomePage(): JSX.Element {
                 <div className={styles.widgetText}>SirageDb</div>
               </a>
             </div>
-          </div>
-          <div>
             <div className={styles.contactWidgetContainer + " " + styles.twitterButton}>
               <a
                 className={styles.contactWidget}
@@ -82,29 +75,30 @@ export default function HomePage(): JSX.Element {
                 <div className={styles.widgetText}>iLoveSemicolon</div>
               </a>
             </div>
-          </div>
-          <div className={styles.contactWidgetContainer + " " + styles.linkedinButton}>
-            <a
-              className={styles.contactWidget}
-              href={"https://www.linkedin.com/in/sirage-al-dbiyat/"}
-              target={"_blank"}
-              rel="noreferrer"
-            >
-              <img
-                className={styles.contatWidgetIcon}
-                src={linkedinIcon}
-                alt={"sirage al dbiyat semicolons linkedin account"}
-              />
-              <div className={styles.widgetText}>Sirage Al Dbiyat</div>
-            </a>
+            <div className={styles.contactWidgetContainer + " " + styles.linkedinButton}>
+              <a
+                className={styles.contactWidget}
+                href={"https://www.linkedin.com/in/sirage-al-dbiyat/"}
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                <img
+                  className={styles.contatWidgetIcon}
+                  src={linkedinIcon}
+                  alt={"sirage al dbiyat semicolons linkedin account"}
+                />
+                <div className={styles.widgetText}>Sirage Al Dbiyat</div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-
       {/* ============================================================================= */}
       <section className={styles.sectionPost}>
         <div className={styles.sectionPostData}>
           <div className={styles.sectionPostSloganContainer}>
+            lighthouse undraw
+            <img className={styles.readerIllustration} src={readerIllustration} alt={"read my posts and tutorials"} />
             <h2 className={styles.sectionPostSlogan}>
               In Semicolons blog you can find posts about various subjects in the tech industry
             </h2>
@@ -132,8 +126,11 @@ export default function HomePage(): JSX.Element {
           </div>
         </div>
       </section>
+
+      {/* style={{ backgroundImage: `url(${blob})` }} */}
       {/* ============================================================================= */}
-      <section className={styles.projectSection}>
+        
+      <section className={styles.projectSection} style={{ backgroundImage: `url(${blob})` }}>
         <div>
           <h2 className={styles.projectSectionTitle}>Technologies and languages i use the most</h2>
         </div>
