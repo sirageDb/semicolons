d86491055a7caa24af752227c25a86107a850dab
SirageDb <sirage@id6net.fr>
2022-01-01 00:45:04 +0100

    FEAT : header bottom border random color

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import { NavLink, Link } from "react-router-dom";
 import styles from "./header.module.scss";
 import logo from "../../assets/logo.png";
 
 
 export default function Header() : JSX.Element {
+
+  const [headerColor, setHeaderColor] = useState("#0067FF")
+
+
+
+  const changeColor = () => {
+    const colors = ["#0067FF", "#FFC356", "#58E1A5", "#6D37DA", "#d35757"]
+    const randomIndex = Math.random() * colors.length;
+    setHeaderColor(colors[Math.floor(randomIndex)]);   
+  }
+
+
+
     return (
-        <header className={styles.headerContainer}>
+        <header onMouseEnter={changeColor} className={styles.headerContainer} style={{borderBottomColor : headerColor}}>
         <div className={styles.header}>
           {/* TODO SEO for image here */}
           <Link to={"/"}> 
