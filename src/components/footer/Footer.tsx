da3da4080868367b23b0b9a3dffb7802fdb47599
SirageDb <sirage@id6net.fr>
2022-01-01 20:00:21 +0100

    CHORE : deleted footer terms and condition

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "../../assets/logo.png";
-import { NavLink } from "react-router-dom";
 import styles from "./footer.module.scss";
 
 export default function Footer(): JSX.Element {
@@ -8,10 +7,7 @@ export default function Footer(): JSX.Element {
     <footer className={styles.footerContainer}>
       <div className={styles.footer}>
         <img alt={"Semicolons website"} src={logo} className={styles.logo} />
-        <div className={styles.footerUtils}>
-          <div>&copy; Semicolons.dev 2022</div>
-          <NavLink to={"www.google.com"}>Terms and conditions</NavLink>
-        </div>
+        <div>&copy;Semicolons.dev 2022</div>
       </div>
     </footer>
   );
