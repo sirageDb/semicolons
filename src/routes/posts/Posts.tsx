87d11aefad099a115848b787ebc9f66e5925a5c0
sirageDb <ilovesemicolons@hotmail.com>
2022-06-03 00:41:42 +0200

    FEAT : posts page heading

--- src/routes/posts/Posts.tsx
+++ src/routes/posts/Posts.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import PageLayout from "../../components/pageLayout/PageLayout";
 import PostCell from "../../components/postCell/PostCell";
-import spaceshipIllustration from "../../assets/spaceshipIllustration.svg";
 import styles from "./posts.module.scss";
 import { IPost } from "../../lib/types";
 import { GET_POSTS_PUBLISHED } from "../../lib/endpoints";
 import Masonry from "react-masonry-css";
+import brand from "../../assets/brand.png";
+
+// My posts are some well developed sidenotes I simply took while working on my real life projects in order to share the problem as well as the solution with you (plus how I found the solution! ).
 
 export default function Posts(): JSX.Element {
   const [posts, setPosts] = useState<IPost[]>();
@@ -23,22 +25,25 @@ export default function Posts(): JSX.Element {
 
   return (
     <PageLayout contentMaxWidth={900}>
-        <div className={styles.pageHeadingContainer}>
-          <div className={styles.pageTitleContainer}>
-            <h1 className={styles.pageTitle}>Posts and articles zone</h1>
-            <h2 className={styles.pageExplanation}>
-              My posts are some well developed sidenotes I simply took while working on my real life projects in order to share the problem as well as the solution with you (plus how I found the solution! ).
-            </h2>
-          </div>
-          <div>
-            <img className={styles.postsImage} src={spaceshipIllustration} alt={"posts and articles page"} />
-          </div>
+      <div className={styles.pageHeadingContainer}>
+        <div className={styles.pageTitleContainer}>
+          <img className={styles.brand} src={brand} alt={"semicolons blog"} />
+          <h1>Blog</h1>
         </div>
+        <h2 className={styles.pageExplanation}>
+          The posts on this page are about some well-developed sidenotes that have been made while working on real-life
+          projects in order to share the problem, its solution, and how it was discovered with you!
+        </h2>
+      </div>
       <div className={styles.postsContainer}>
-        <Masonry breakpointCols={{default : 2, 576:1}} className={styles.myMasonryGrid} columnClassName={styles.myMasonryGridColumn}>
+        <Masonry
+          breakpointCols={{ default: 2, 576: 1 }}
+          className={styles.myMasonryGrid}
+          columnClassName={styles.myMasonryGridColumn}
+        >
           {posts?.map((post: IPost, i: number) => {
             return (
-              <div  className={styles.singlePostCellContainer} key={i}>
+              <div className={styles.singlePostCellContainer} key={i}>
                 <PostCell
                   _id={post?._id}
                   imagePath={post?.image?.path}
--- src/routes/projects/Projects.tsx
+++ src/routes/projects/Projects.tsx
@@ -1,46 +1,40 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import PageLayout from "../../components/pageLayout/PageLayout";
 import ProjectCell from "../../components/projectCell/ProjectCell";
-import spacemanProjects from "../../assets/spacemanProjects.svg";
 import styles from "./projects.module.scss";
 import { IProject } from "../../lib/types";
 import { GET_PROJECTS_PUBLISHED } from "../../lib/endpoints";
+import brand from "../../assets/brand.png";
 
 export default function Projects(): JSX.Element {
-
-
   const [projects, setProject] = useState<IProject[]>([]);
-  
+
   useEffect(() => {
     fetchProjects();
-  }, [])
+  }, []);
 
   const fetchProjects = async () => {
     const apiResponse = await fetch(GET_PROJECTS_PUBLISHED);
     const data = await apiResponse.json();
     setProject(data);
-  }
-  
+  };
+
   return (
     <PageLayout contentMaxWidth={900}>
-      <div className={styles.headingContainer}>
-        <div className={styles.pageHeadingContainer}>
-          <div className={styles.pageTitleContainer}>
-            <h1 className={styles.pageTitle}>Projects zone</h1>
-            <h2 className={styles.pageExplanation}>
-              My projects and my side projects..., have a look, some of them might interest you ;-)</h2>
-          </div>
-          <div className={styles.illustrationContainer}>
-            <img className={styles.projectsImage} src={spacemanProjects} alt={"posts and articles page"} />
-          </div>
+      <div className={styles.pageHeadingContainer}>
+        <div className={styles.pageTitleContainer}>
+          <img className={styles.brand} src={brand} alt={"semicolons projects and use case"} />
+          <h1>Projects and case study</h1>
         </div>
+        <h2 className={styles.pageExplanation}>
+          Dive in case studies of our projects, side projects and some opensource.
+        </h2>
       </div>
 
-
       <div className={styles.projectsContainer}>
-        {
-          projects.map((project, index : number) => {
-            return (<ProjectCell 
+        {projects.map((project, index: number) => {
+          return (
+            <ProjectCell
               key={index}
               image={project.image.path}
               imageAlt={project.image.alt}
@@ -49,9 +43,9 @@ export default function Projects(): JSX.Element {
               projectLink={project.projectLink}
               sourceCodeLink={project.sourceCodeLink}
               description={project.description}
-            />)
-          })
-        }
+            />
+          );
+        })}
       </div>
     </PageLayout>
   );
