1906d110d6a5675f91df9a933690dca0ec73d166
sirageDb <ilovesemicolons@hotmail.com>
2022-06-05 09:13:01 +0200

    CHORE : API END POINT

--- src/config/apiEndPoint.ts
+++ src/config/apiEndPoint.ts
@@ -2,5 +2,5 @@ const endpoitns = {
     api_dev: "http://localhost:5000",
     api_prod : "https://api.semicolons.dev"
 }
-const apiEndPoint = endpoitns.api_dev; 
+const apiEndPoint = endpoitns.api_prod; 
 export default apiEndPoint;
